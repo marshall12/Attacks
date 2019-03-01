66e40fc2f42b3f5fa6ace283b11d02e4caa65f68
marshall12 <slimshaddy.d@gmail.com>
2019-03-01 12:43:14 +0545

    Create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for,jsonify
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return '<h1>LOGIN SUCCESSFUL</h1>'
+
+
+@app.route('/login1', methods=['GET','POST'])
+def login1():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != '00000' or request.form['password'] != '00005':
+            error = 'Invalid username or password. Please try again'
+            return jsonify({'message': error, 'success': False})
+
+        else:
+            return jsonify({'success': True})
+            return redirect(url_for('home'))
+
+
+    return render_template('login.html', error=error)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
